34c7369786b8f85a10f59a404d28d8065c40141d
Am10aN16 <2020071013@mmmut.ac.in>
2022-08-08 11:21:10 +0530

    2nd

--- array.cpp
+++ array.cpp
@@ -1,1 +1,41 @@
 #include<iostream>
+using namespace std;
+
+int main(){
+
+//array declare
+int arr[15];
+
+//accessing the value of array at nth position
+cout<< arr[14] << endl;
+
+//initialization Array
+int sec[3] = {5,7,11};
+
+//accessing the value of array 
+cout<< sec[2] << endl;
+
+int third[15] = {2,7};
+
+int n=15;
+
+//printing the array
+for (int i = 0; i < n; i++)
+{
+    cout<< third[i] << " ";
+}
+
+cout<< endl;
+int fourth[10] = {1};
+
+int m=10;
+
+//printing the array
+for (int i = 0; i < m; i++)
+{
+    cout<< fourth[i] << " ";
+}
+
+return 0;
+
+}
