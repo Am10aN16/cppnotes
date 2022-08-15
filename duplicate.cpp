334e16d08671d7d36ded3766c22bbaf80fe78cda
Am10aN16 <2020071013@mmmut.ac.in>
2022-08-15 11:44:22 +0530

    duplicate

---
+++ duplicate.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int findDuplicate(int arr[] , int size) {
+    int ans = 0 ; 
+
+    for(int i = 0; i < size ; i++){
+        ans = ans ^ arr[i];
+    };
+
+    for(int i =1 ; i< size ; i++){
+        ans = ans^i;
+    };
+    return ans;
+}
+
+int main(){
+int arr[10] = {1,2,3,4,5,6,7,8,1,9};
+cout<< findDuplicate(arr , 10);
+};
