09573e9416431b0504508c31b811bb65f1e44055
Am10aN16 <2020071013@mmmut.ac.in>
2022-09-04 00:08:36 +0530

    selection

---
+++ bubblesort.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(vector<int>& arr, int n)
+{   
+    for(int i =1 ; i< n ; i++){
+        for(int j =0 ; j< n-i ; j++){
+            if(arr[j]> arr[j+1]){
+                swap(arr[j] , arr[j+1]);
+            };
+        };
+    };
+};
