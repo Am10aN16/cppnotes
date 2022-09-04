dea845ee8cf9fc0ac30b60f5fabf03a0145e2aa5
Am10aN16 <2020071013@mmmut.ac.in>
2022-09-05 00:20:43 +0530

    insertion

---
+++ insertionsort.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int n, vector<int> &arr){
+    for(int i =1 ; i < n ; i++){
+        int temp = arr[i];
+        int j = i-1;
+        while(j>=0){
+            if(arr[j] > temp){
+                arr[j+1] = arr[j];
+            }
+            else{
+                break;
+            }
+            j--;
+        }
+        arr[j+1] = temp;
+    }
+}
