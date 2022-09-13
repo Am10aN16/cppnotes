2bc95a12fe96b50d9faccfb8b23ee7d987683a93
Am10aN16 <2020071013@mmmut.ac.in>
2022-09-14 00:30:48 +0530

    2ques

---
+++ rotatearray.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ void rotate(vector<int>& nums, int k) {
+        vector<int> ans(nums.size());
+        for(int i = 0 ; i< nums.size();i++){
+            
+            ans[(i + k) % nums.size()] = nums[i];
+        }
+        
+        nums = ans;
+    }
