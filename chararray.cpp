d97765b50c495fdd05d061e4ba1bcf7bf048e190
Am10aN16 <2020071013@mmmut.ac.in>
2022-09-25 11:31:01 +0530

    que added

--- chararray.cpp
+++ chararray.cpp
@@ -1,12 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int getLength(char name[]){
+    int count =0;
+    for(int i=0;name[i] != 0 ; i++){
+        count++;
+    }
+
+    return count;
+}
+
 int main(){
     char name[20];
     cout<<"Enter your name: "<<endl;
     cin>>name;
-    name[2] = '\0';
+    // name[2] = '\0';
     cout<<"Your name is:- "<<name<<endl;
+    cout<<"Length of your char array is:- "<<getLength(name)<<endl;
 
     return 0;
 }
