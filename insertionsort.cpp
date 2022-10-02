0e172ec38f2ff14cc3ec9a62818ba40714719e92
Am10aN16 <2020071013@mmmut.ac.in>
2022-10-02 17:13:44 +0530

    quessol

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void insertionSort(int n, vector<int> &arr){
+void insertionSort(int arr[] , int n ){
     for(int i =1 ; i < n ; i++){
         int temp = arr[i];
         int j = i-1;
@@ -19,7 +19,15 @@ void insertionSort(int n, vector<int> &arr){
 }
 
 int main(){
+    int arr[6] = {10,15,7,2,1,9};
     
+    insertionSort(arr , 6);
+
+    for(int i =0 ;i< 6; i++)
+   {
+    cout<<arr[i]<<",";
+   }
+
 
     return 0;
 }
