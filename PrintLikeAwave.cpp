bc34e8259a227ce774a01887a935efccde0e7cf3
Am10aN16 <2020071013@mmmut.ac.in>
2022-11-30 09:16:32 +0530

    1ques added on 2Darray

---
+++ PrintLikeAwave.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//function for printing like a wave
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    vector<int> ans;
+    for(int col = 0; col<mCols; col++){
+        if(col&1){
+            //odd index case
+            for(int row = nRows-1; row>=0;row--){
+                ans.push_back(arr[row][col]);
+            }
+            
+        }else{
+            //even index case
+             for(int row = 0; row<nRows ;row++){
+                 ans.push_back(arr[row][col]);
+             }
+        }   
+    }
+    return ans;
+}
