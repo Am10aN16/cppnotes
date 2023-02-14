fbe55d0493cee161bb8ff311640c5e8a514e0e5d
Am10aN16 <2020071013@mmmut.ac.in>
2023-02-14 22:52:33 +0530

    new phase

---
+++ phase2/pointer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 5;
+    cout<<num<<endl;
+
+    //address of operator- &
+    cout<<"Address of num operator is: "<<&num<<endl;
+    return 0;
+
+}
