a30ce121d31c405695578ede8c30d03051eee9d6
Am10aN16 <2020071013@mmmut.ac.in>
2023-03-09 14:23:51 +0530

    new

--- phase2/charpointer.cpp
+++ phase2/charpointer.cpp
@@ -6,10 +6,14 @@ int main(){
 int arr[5] = {1,2,3,4,5};
 char ch[6] = "abcde";
 
- cout<<arr<<endl;
- cout<<ch<<endl;
+//  cout<<arr<<endl;
+//  cout<<ch<<endl;
 
+int *p = &arr[0];
  char *c = &ch[0];
+ //print only first address
+ cout<<p<<endl; 
+ //print whole array elements
  cout<<c<<endl; 
 
     return 0;
