903cfd07e633d474b57f30d49341073821fb274c
Am10aN16 <2020071013@mmmut.ac.in>
2023-03-26 16:21:49 +0530

    newadded

---
+++ phase2/referencevar.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void update(int& n){
+    n++;
+}
+
+int main(){
+
+    // int i =5;
+    // int& j = i;
+
+    // cout<<i<<endl;
+    // i++;
+    // cout<<i<<endl;
+    // j++;
+    // cout<<i<<endl;
+    int n =5;
+    cout<<"Before"<<n<<endl;
+    update(n);
+    cout<<"After"<<n<<endl;
+
+    return 0;
+}
