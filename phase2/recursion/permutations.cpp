585529e3e2cae623dd092a1c6ed5bbd64b1e5f13
Am10aN16 <2020071013@mmmut.ac.in>
2023-06-13 16:27:19 +0530

    new

---
+++ phase2/recursion/permutations.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    void solve(vector<int> nums, int index, vector<vector<int>>& ans){
+    //base case
+    if(index >= nums.size()){
+    ans.push_back(nums);
+    return;
+        }
+
+    for(int j = index; j<nums.size(); j++){
+        swap(nums[index], nums[j]);
+        solve(nums,index+1,ans);
+        //backtrack
+         swap(nums[index], nums[j]);
+    }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index =0;
+        solve(nums, index , ans);
+        return ans;
+    }
+};
+ 
+int main()
+{
+    return 0;
+}
+
+//leetcode 46Ques
