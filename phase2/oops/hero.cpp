92e019d6465502b9bdef79f72990e25eb3d54185
Am10aN16 <2020071013@mmmut.ac.in>
2023-06-15 01:16:04 +0530

    new

---
+++ phase2/oops/hero.cpp
@@ -0,0 +1,6 @@
+class Hero{
+    //properties
+    char name[100];
+    int health;
+    char level;
+};
---
+++ phase2/oops/intro.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include"hero.cpp"
+using namespace std;
+ 
+int main()
+{
+Hero h1;
+
+cout<<"Size is: "<<sizeof(h1)<<endl; 
+ 
+    return 0;
+}
