470252cfa5de694bc36aafab795e5b9e3e673da9
Am10aN16 <2020071013@mmmut.ac.in>
2023-07-21 01:48:33 +0530

    new

--- phase2/stack/stackusingLL.cpp
+++ phase2/stack/stackusingLL.cpp
@@ -12,6 +12,68 @@ public:
         this->val = val;
         this->next = NULL;
     }
+
+    void push(int d, Stack *&head, Stack *&tail)
+    {
+        Stack *newNode = new Stack(d);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void pop(Stack *&head, Stack *&tail)
+    {
+        if (head == NULL)
+        {
+            cout << "Stack underflow" << endl;
+        }
+        else
+        {
+            if (head == tail)
+            {
+                head = NULL;
+                return;
+            }
+
+            Stack *temp = head;
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+            temp->next = NULL;
+            tail = temp;
+        }
+    }
+
+    int peek(Stack *&tail)
+    {
+        if (tail == NULL)
+        {
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+        else
+        {
+            return tail->val;
+        }
+    }
+
+    bool empty(Stack *&head)
+    {
+        if (head == NULL)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
 };
 
 int main()
