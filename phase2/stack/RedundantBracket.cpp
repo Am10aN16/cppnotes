8a5c1ff99b4f6e9b34a4b920e2e59a89c36fc954
Am10aN16 <2020071013@mmmut.ac.in>
2023-07-28 01:31:17 +0530

    new

---
+++ phase2/stack/RedundantBracket.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+bool findRedundantBrackets(string &s)
+{
+    // Write your code here.
+    stack<char> st;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        char ch = s[i];
+
+        if (ch == '(' || ch == '+' || ch == '-' || ch == '*' || ch == '/')
+        {
+            st.push(ch);
+        }
+        else
+        {
+            if (ch == ')')
+            {
+                bool isReduntant = true;
+                while (st.top() != '(')
+                {
+                    char top = st.top();
+
+                    if (top == '+' || top == '-' || top == '*' || top == '/')
+                    {
+                        isReduntant = false;
+                    }
+                    st.pop();
+                }
+                if (isReduntant == true)
+                    return true;
+                st.pop();
+            }
+        }
+    }
+    return false;
+}
